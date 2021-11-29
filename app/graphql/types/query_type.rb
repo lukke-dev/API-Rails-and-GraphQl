77916f5ec8224f4b7e25a6117199c8f6c4d7331b
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-11-29 04:30:02 -0300

    Added show and index users

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,24 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
+    field :users, [Types::UserType], null: false
+
+    def users
+      User.all
+    end
+
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
+
     field :test_field, String, null: false,
-      description: "An example field added by the generator"
+                               description: 'An example field added by the generator'
     def test_field
-      "Hello World!"
+      'Hello World!'
     end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,7 +3,13 @@ module Types
     field :id, ID, null: false
     field :email, String, null: true
     field :name, String, null: true
+    field :books, [Types::BookType], null: true
+    field :books_count, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def books_count
+      object.books.size
+    end
   end
 end
