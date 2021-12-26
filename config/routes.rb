743bd101fa1105040ea6228271edfd1e5b89ded9
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-12-26 17:50:30 -0300

    allow IDE graphiql

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute' if Rails.env.development?
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute'
   post '/graphql', to: 'graphql#execute'
 end
